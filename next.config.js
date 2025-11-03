d442b7a30da743e8cbc79bc578aa30468593fd6e
angelinastrelnikava34-cloud <angelinastrelnikava34@gmail.com>
2025-11-03 15:31:42 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { domains: ['images.unsplash.com'] } };
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' }, // можно оставить
+      { protocol: 'https', hostname: 'drive.google.com' }     // добавили Drive
+    ],
+  },
+};
+
 module.exports = nextConfig;
